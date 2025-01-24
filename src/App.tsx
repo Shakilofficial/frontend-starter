cb1123a91f021c99bce20c45ee252dabc0ec4d74
Shakilofficial <mrshakilhossainofficial@gmail.com>
2025-01-24 19:49:04 +0600

    done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is the App component</h1>
+      <Button>Add Me</Button>
+    </div>
+  );
+};
+
+export default App;
